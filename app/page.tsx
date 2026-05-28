06b335d8d6169b0eac495d53a859b8b34a70b019
BRET <thorkell027@gmail.com>
2026-05-28 04:29:56 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -544,8 +544,9 @@ My journey in IT was driven by curiousity and willingness to learn new technolog
               </div>
             </div>
 
+             <div className="space-y-6">
             {/* Brgy Waste Management System */}
-            <div className="space-y-6">
+           
             <div className="glass-card p-6">
               <div className="flex flex-col md:flex-row md:items-start md:justify-between gap-4">
                 <div className="flex-1">
@@ -585,6 +586,7 @@ My journey in IT was driven by curiousity and willingness to learn new technolog
                 </div>
               </div>
             </div>
+          </div>
           </div>
           </div>
         </section>
